b430c9d0eac6c07c0fe4253a8dba364b769a174a
MK <67931479+TheOtherMK@users.noreply.github.com>
2022-09-05 10:57:12 +0100

    added basic maths functions

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+let num1;
+let num2;
+let result;
 
+function operate(action) {
+
+    if (action === 'add'){
+
+        result = num1 + num2;
+       
+    }
+    else if (action === 'subtract'){
+        result = num1 - num2;
+        
+    }
+    else if (action === 'multiply'){
+        result = num1 * num2;
+        
+    }
+    else if (action === 'divide'){
+        result = num1 / num2;
+        
+    }
+    return result;
+
+}
