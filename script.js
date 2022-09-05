0dd3f07b6856cea4716eb1f1a11fb77d4ec74329
MK <67931479+TheOtherMK@users.noreply.github.com>
2022-09-05 13:52:22 +0100

    pressed buttons show on display

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let num1;
 let num2;
 let result;
+let dis;
+
+function getValue(choice){
+    dis = choice;
+console.log(dis);
+}
+
+function sayHello() {
+
+    document.getElementById('display').innerHTML = dis;
+    console.log(dis);
+   
+}
 
 function operate(action) {
 
@@ -23,4 +36,13 @@ function operate(action) {
     }
     return result;
 
+}
+
+function storeNumber(num) {
+    let num1 = num.value;
+    console.log(num1);
+}
+
+function getResults(){
+    //something something get results from operate() and display them in a display window
 }
