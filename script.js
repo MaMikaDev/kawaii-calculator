3b92c74f69da8786a48e13e75375b596704a5957
MK <67931479+TheOtherMK@users.noreply.github.com>
2022-09-06 18:36:27 +0100

    all functions working properly

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let previous;
 let current;
 
-let result;
+let result = null;
 let value;
 
 let num1 = null;
@@ -19,6 +19,8 @@ function clearThis() {
   document.getElementById("secondaryDisplay").innerHTML = "";
   document.getElementById("primaryDisplay").innerHTML = "";
   console.log("cleared");
+  num1 = null;
+  result = null;
 }
 
 function getValue(choice) {
@@ -26,69 +28,71 @@ function getValue(choice) {
 }
 
 function sayHello() {
+  console.log("value is " + value);
 
-
-  
-
-  previous = value; /// adds the number upon pressing the button    
+  previous = value; /// adds the number upon pressing the button
   current = document.getElementById("primaryDisplay").innerHTML += previous; //creates the first number for calculation
+
+  console.log("say hello is current: " + current);
+  console.log("current num in hello function " + num1);
 }
 
 function operator(choice) {
+  operate();
   buttonsBackOnline();
 
   theOperator = choice; // identify what the operator is (add, subtract, multiply or divide)
+  console.log(theOperator);
 
-  if (num1 != null) {
-    //checks if num1 is already 'filled'
+  if (result != null) {
+    //checks if there is already an existing result
 
-    console.log("num1 is not null");
-    operate(); //perfoms a basic calculation and returns the result
+    console.log("result is not null");
 
-    num1 = result; //result becomes a new num1, ready for another calculation involving it
-    console.log("this is new num1 " + num1);
-    document.getElementById("secondaryDisplay").innerHTML = num1 + " " + value;
-    document.getElementById("primaryDisplay").innerHTML = "";
+    num1 = result;
+
     buttonsBackOnline(); //makes sure the number buttons are unblocked
-
   } else {
-
     console.log("current num " + num1);
 
     num1 = current; //save the first number
-
-    document.getElementById("secondaryDisplay").innerHTML = num1 + " " + value; // send the first number to secondary display along with the value
-    document.getElementById("primaryDisplay").innerHTML = ""; //clear the primary display ready for next number
     previous = 0;
-  
-}
+  }
+
+  document.getElementById("secondaryDisplay").innerHTML = num1 + " " + value; // send the first number to secondary display along with the value
+  document.getElementById("primaryDisplay").innerHTML = ""; //clear the primary display ready for next number
+
+  operate();
+  buttonsBackOnline();
+  previous = 0;
   // need to add a button to stop from pressing ' . ' button more than once to prevent errors
 }
 
 function operate() {
-  switch (theOperator) {
-    case "add":
-      result = +num1 + +current; // ensures the values get added not concatenated
-      break;
-      console.log(result);
+  if (num1 === null || current === null) {
+    return;
+  } else {
+    switch (theOperator) {
+      case "add":
+        result = +num1 + +current; // ensures the values get added not concatenated
+        break;
 
-    case "subtract":
-      result = num1 - current;
-      break;
+      case "subtract":
+        result = num1 - current;
+        break;
 
-    case "multiply":
-      result = num1 * current;
-      break;
+      case "multiply":
+        result = num1 * current;
+        break;
 
-    case "divide":
-      result = num1 / current;
-      break;
+      case "divide":
+        result = num1 / current;
+        break;
 
-    default:
-      result = "error";
+      default:
+        result = "error";
+    }
   }
-
-  current = result; // result gets moved to a current number.
 
   tempDisableButtons();
 }
