b458aad99ac2f2c82aa8afc5e0ab4a475ab9edb3
MK <67931479+TheOtherMK@users.noreply.github.com>
2022-09-06 18:44:19 +0100

    added commentary and cleaned up the code

--- script.js
+++ script.js
@@ -14,11 +14,13 @@ function getResults() {
 }
 
 function clearThis() {
+  // unlock the number buttons
   buttonsBackOnline();
   // clear secondary and primary displays
   document.getElementById("secondaryDisplay").innerHTML = "";
   document.getElementById("primaryDisplay").innerHTML = "";
   console.log("cleared");
+  // ensure the num1 and result properties are cleared too
   num1 = null;
   result = null;
 }
@@ -28,53 +30,47 @@ function getValue(choice) {
 }
 
 function sayHello() {
-  console.log("value is " + value);
 
   previous = value; /// adds the number upon pressing the button
   current = document.getElementById("primaryDisplay").innerHTML += previous; //creates the first number for calculation
 
-  console.log("say hello is current: " + current);
-  console.log("current num in hello function " + num1);
 }
 
 function operator(choice) {
-  operate();
-  buttonsBackOnline();
+  operate(); //perform an up to date calculation
+  buttonsBackOnline(); // ensure the number buttons are unlocked
 
   theOperator = choice; // identify what the operator is (add, subtract, multiply or divide)
-  console.log(theOperator);
 
   if (result != null) {
     //checks if there is already an existing result
 
-    console.log("result is not null");
+    num1 = result; 
 
-    num1 = result;
-
-    buttonsBackOnline(); //makes sure the number buttons are unblocked
   } else {
-    console.log("current num " + num1);
-
-    num1 = current; //save the first number
-    previous = 0;
+    
+    num1 = current; // save the first number
+    previous = 0; // clean the slate for the next one
   }
 
   document.getElementById("secondaryDisplay").innerHTML = num1 + " " + value; // send the first number to secondary display along with the value
   document.getElementById("primaryDisplay").innerHTML = ""; //clear the primary display ready for next number
 
-  operate();
-  buttonsBackOnline();
+  operate(); // update the latest result
+  buttonsBackOnline(); //ensure the number buttons are unlocked
   previous = 0;
-  // need to add a button to stop from pressing ' . ' button more than once to prevent errors
+  // need to potentially add a button to stop from pressing ' . ' button more than once to prevent errors
 }
 
 function operate() {
   if (num1 === null || current === null) {
-    return;
-  } else {
+
+    return; // checks if there are two numbers if not, returns without further action
+  
+} else {
     switch (theOperator) {
       case "add":
-        result = +num1 + +current; // ensures the values get added not concatenated
+        result = +num1 + +current; // ensures the values get added not concatenated into a string
         break;
 
       case "subtract":
@@ -94,7 +90,7 @@ function operate() {
     }
   }
 
-  tempDisableButtons();
+  tempDisableButtons(); // to avoid errors and force user to use an operate or clear button
 }
 
 function tempDisableButtons() {
